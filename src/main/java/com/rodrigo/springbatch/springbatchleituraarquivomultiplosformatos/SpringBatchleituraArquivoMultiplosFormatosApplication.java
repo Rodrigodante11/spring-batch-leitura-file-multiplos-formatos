11718ba96ff3dfc7823d3a1fb306379aba440bef
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2024-06-30 14:09:45 -0300

    start project

---
+++ src/main/java/com/rodrigo/springbatch/springbatchleituraarquivomultiplosformatos/SpringBatchleituraArquivoMultiplosFormatosApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigo.springbatch.springbatchleituraarquivomultiplosformatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchleituraArquivoMultiplosFormatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchleituraArquivoMultiplosFormatosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigo/springbatch/springbatchleituraarquivomultiplosformatos/SpringBatchleituraArquivoMultiplosFormatosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigo.springbatch.springbatchleituraarquivomultiplosformatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchleituraArquivoMultiplosFormatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
